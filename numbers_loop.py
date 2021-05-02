dac9c3a056360dbe46f5ac13d0965fc709c8e68a
200727379 <rampedilesley@gmail.com>
2021-05-02 13:09:28 -0700

    initial commit

---
+++ numbers_loop.py
@@ -0,0 +1,17 @@
+#Author: Mothapo Rampedi Lesley
+#Email: rampedilesley@gmail.com
+#02 May 2021
+"""
+Print First 10 natural numbers using while loop and again using for loop
+"""
+i = 1
+
+print("while Loop Results")
+while i <= 10:
+    print(i)
+    i += 1
+
+
+print("for Loop Results")
+for x in range(11):
+    print(x)
